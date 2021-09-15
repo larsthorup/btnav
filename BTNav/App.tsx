f6dc543919f7a3e465ef7f718516945450d265b0
Lars Thorup <lars@zealake.com>
2021-09-15 15:03:56 +0200

    compass

--- BTNav/App.tsx
+++ BTNav/App.tsx
@@ -8,10 +8,11 @@
  * @format
  */
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
+  Image,
   Text,
   useColorScheme,
   View,
@@ -19,36 +20,65 @@ import {
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
+import CompassHeading from 'react-native-compass-heading';
+
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
+  const [compassHeading, setCompassHeading] = useState(0);
+
+  useEffect(() => {
+    const degree_update_rate = 3;
+
+    // accuracy on android will be hardcoded to 1
+    // since the value is not available.
+    // For iOS, it is in degrees
+    CompassHeading.start(degree_update_rate, ({heading, accuracy}) => {
+      console.log({heading});
+      setCompassHeading(heading);
+    });
+
+    return () => {
+      CompassHeading.stop();
+    };
+  }, []);
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <View style={styles.sectionContainer}>
-        <Text
-          style={[
-            styles.sectionTitle,
-            {
-              color: isDarkMode ? Colors.white : Colors.black,
-            },
-          ]}>
-          SteadyNav Demo
-        </Text>
-        <Text
-          style={[
-            styles.sectionDescription,
-            {
-              color: isDarkMode ? Colors.light : Colors.dark,
-            },
-          ]}>
-          some text
-        </Text>
-      </View>
-    </SafeAreaView>
+    <>
+      <SafeAreaView style={backgroundStyle}>
+        <View style={styles.sectionContainer}>
+          <Text
+            style={[
+              styles.sectionTitle,
+              {
+                color: isDarkMode ? Colors.white : Colors.black,
+              },
+            ]}>
+            SteadyNav Demo
+          </Text>
+          <Text
+            style={[
+              styles.sectionDescription,
+              {
+                color: isDarkMode ? Colors.light : Colors.dark,
+              },
+            ]}>
+            some text
+          </Text>
+        </View>
+      </SafeAreaView>
+      <Image
+        style={[
+          styles.compass,
+          {transform: [{rotate: `${360 - compassHeading}deg`}]},
+        ]}
+        resizeMode="contain"
+        source={require('./compass.png')}
+      />
+    </>
   );
 };
 
@@ -65,6 +95,11 @@ const styles = StyleSheet.create({
     marginTop: 8,
     fontSize: 18,
     fontWeight: '400',
+  },
+  compass: {
+    width: '90%',
+    flex: 1,
+    alignSelf: 'center',
   },
 });
 
