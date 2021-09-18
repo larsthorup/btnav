676a32da4bef9c025445702cab0669d1aea5e7b8
Lars Thorup <lars@zealake.com>
2021-09-18 13:59:27 +0200

    logrocket for telemetry

--- BTNav/index.js
+++ BTNav/index.js
@@ -3,7 +3,11 @@
  */
 
 import { AppRegistry } from 'react-native';
+import LogRocket from '@logrocket/react-native';
+
 import App from './src/App';
 import { name as appName } from './app.json';
+
+LogRocket.init('h5ovjy/btnav-dev')
 
 AppRegistry.registerComponent(appName, () => App);
--- README.md
+++ README.md
@@ -11,3 +11,7 @@ yarn android # in terminal 2
 yarn android:build
 yarn android:install
 ```
+
+## Telemetry
+
+https://app.logrocket.com/h5ovjy/btnav-dev
